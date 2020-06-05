69bc2caec51c52e5d11977d3914cdea5d6bd0cfa
es0 <carlsonj@ring-zer0.com>
2020-06-05 12:16:33 -0700

    Implement RSS feed for blog

--- my_site/blog/urls.py
+++ my_site/blog/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include, re_path
 from . import views
+from .feeds import LatestPostFeed
 
 app_name = 'blog'
 urlpatterns = [
@@ -8,4 +9,5 @@ urlpatterns = [
     re_path(r'^tag/(?P<tag_slug>[-\w]+)/$', views.post_list, name='post_list_by_tag'),
     re_path(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<post>[-\w]+)/$',views.post_detail, name="post_detail"),
     re_path(r'^(?P<post_id>\d+)/share/$', views.post_share, name='post_share'),
+    re_path(r'^feed/$', LatestPostFeed(), name='post_feed'),
     ]
