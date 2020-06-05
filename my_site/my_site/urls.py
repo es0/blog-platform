aed43fb7caab29e7edeee45c3403120ea50f0586
es0 <carlsonj@ring-zer0.com>
2020-06-04 21:57:17 -0700

    Add sitemap.xml generation

--- my_site/my_site/urls.py
+++ my_site/my_site/urls.py
@@ -15,9 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include, re_path
-
+from django.contrib.sitemaps.views import sitemap
+from blog.sitemaps import PostSitemap
+sitemaps = {
+    'posts': PostSitemap,
+}
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     re_path(r'^blog/', include('blog.urls', 'blog')),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
 ]
