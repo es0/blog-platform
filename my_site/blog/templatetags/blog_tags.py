c430aacd99a1b4ad9616e95735ff358641476300
es0 <carlsonj@ring-zer0.com>
2020-06-04 21:21:50 -0700

    Fix a Markdown formatting problem. May not be fully fixed..

--- my_site/blog/templatetags/blog_tags.py
+++ my_site/blog/templatetags/blog_tags.py
@@ -1,10 +1,12 @@
 from django import template
-from ..models import Post
 from django.db.models import Count
 from django.utils.safestring import mark_safe
 import markdown
 
 register = template.Library()
+from ..models import Post
+
+
 @register.simple_tag
 def total_posts():
     return Post.published.count()
